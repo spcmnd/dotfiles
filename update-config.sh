ab0e5f15f6e801f6e1b83e541cf748b38c4a17cf
spcmnd <spcmndd@protonmail.com>
2021-10-18 13:36:45 +0200

    Update tmux conf and script

--- update-config.sh
+++ update-config.sh
@@ -5,29 +5,66 @@ homeDirectory=$HOME
 
 pull_latest_version () {
 	echo
-	echo '[*] Pulling latest repository version...'
+	echo "[*] Pulling latest repository version..."
 	git pull > /dev/null
-	echo '[*] Done.'
+	echo "[*] Done."
 }
 
 update_tmux_conf () {
+	tmuxLoggingPluginPath="$HOME/Documents/tmux-logging"
+
 	echo
-	echo '[*] Updating tmux configuration...'
+	echo "[*] Updating tmux configuration..."
 	cp -f $currentDirectory/.tmux.conf $homeDirectory/.tmux.conf
-	echo '[*] Tmux configuration updated!'
+	echo "[*] Tmux configuration updated!"
+
+	if [ ! -d "$tmuxLoggingPluginPath" ]; then
+		echo "[*] Tmux logging pluggin is not present."
+
+		while true; do
+			read -p "[*] Do you want to import it? [Y/N] " yn
+
+			case $yn in
+				[Yy]* )
+					echo "[*] Cloning tmux-logging..."
+					git clone https://github.com/tmux-plugins/tmux-logging.git $tmuxLoggingPluginPath > /dev/null 2>&1
+					echo "[*] Done."
+					break
+					;;
+				[Nn]* )
+					break
+					;;
+				* )
+					break
+					;;
+			esac
+		done
+	fi
+
+	echo "[*] Loading updated Tmux configuration..."
+	tmux source-file "$HOME/.tmux.conf" > /dev/null 2>&1
+	echo "[*] Configuration loaded!"
 }
 
 main () {
-	echo '---'
-	echo 'Config Updater - V1.0 - By spcmnd'
-	echo '---'
+	echo "---"
+	echo "Config Updater - V1.0 - By spcmnd"
+	echo "---"
 	while true; do
 		read -p "Do you want to update Tmux configuration? [Y/N] " yn
 
 		case $yn in
-			[Yy]* ) pull_latest_version; update_tmux_conf; break;;
-			[Nn]* ) break;;
-			* ) echo 'Please answer Y or N.';;
+			[Yy]* ) 
+				pull_latest_version
+				update_tmux_conf
+				break
+				;;
+			[Nn]* ) 
+				break
+				;;
+			* ) 
+				echo "Please answer Y or N."
+				;;
 		esac
 	done
 }
