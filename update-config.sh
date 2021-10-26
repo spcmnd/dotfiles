b5f1c95b4018a02866ea3e237806a3ce4db9984e
spcmnd <spcmndd@protonmail.com>
2021-10-26 15:59:02 -0400

    Add aliases and bashrc config

--- update-config.sh
+++ update-config.sh
@@ -3,11 +3,50 @@
 currentDirectory=$PWD
 homeDirectory=$HOME
 
+ask_pull_latest_version () {
+	while true; do
+		read -p "Do you want to pull the latest version of files? [Y/N] " yn
+
+		case $yn in
+			[Yy]* )
+				pull_latest_version
+				break
+				;;
+			[Nn]* )
+				break
+				;;
+			* )
+				echo "Please answer Y or N."
+				;;
+		esac
+	done
+}
+
 pull_latest_version () {
 	echo
 	echo "[*] Pulling latest repository version..."
 	git pull > /dev/null
 	echo "[*] Done."
+	echo
+}
+
+ask_update_tmux_conf () {
+	while true; do
+		read -p "Do you want to update Tmux configuration? [Y/N] " yn
+
+		case $yn in
+			[Yy]* )
+				update_tmux_conf
+				break
+				;;
+			[Nn]* )
+				break
+				;;
+			* )
+				echo "Please answer Y or N."
+				;;
+		esac
+	done
 }
 
 update_tmux_conf () {
@@ -44,29 +83,61 @@ update_tmux_conf () {
 	echo "[*] Loading updated Tmux configuration..."
 	tmux source-file "$HOME/.tmux.conf" > /dev/null 2>&1
 	echo "[*] Configuration loaded!"
+	echo "[*] Done."
+	echo
+}
+
+ask_update_bashrc () {
+	while true; do
+		read -p "Do you want to update your bashrc? [Y/N] " yn
+
+		case $yn in
+			[Yy]* )
+				update_bashrc
+				break
+				;;
+			[Nn]* )
+				break
+				;;
+			* )
+				echo "Please answer Y or N."
+				;;
+		esac
+	done
+}
+
+update_bashrc () {
+	bashrcPath=$homeDirectory/.bashrc
+
+	if grep -Fxq "# spcmnd's custom config" $bashrcPath
+	then
+    echo
+		echo "[*] Config is already loaded!"
+	else
+		echo
+		echo "[*] Adding custom config to .bashrc..."
+		echo "" >> $bashrcPath
+		echo "# spcmnd's custom config" >> $bashrcPath
+		echo "source \"$homeDirectory/Documents/dotfiles/.bashrc\"" >> $bashrcPath
+		echo "" >> $bashrcPath
+	fi
+
+	echo "[*] Done."
+	echo
 }
 
 main () {
 	echo "---"
 	echo "Config Updater - V1.0 - By spcmnd"
 	echo "---"
-	while true; do
-		read -p "Do you want to update Tmux configuration? [Y/N] " yn
+	echo
+	ask_pull_latest_version
+	ask_update_tmux_conf
 
-		case $yn in
-			[Yy]* ) 
-				pull_latest_version
-				update_tmux_conf
-				break
-				;;
-			[Nn]* ) 
-				break
-				;;
-			* ) 
-				echo "Please answer Y or N."
-				;;
-		esac
-	done
+	if ! grep -Fxq "# spcmnd's custom config" $homeDirectory/.bashrc
+	then
+		ask_update_bashrc
+	fi
 }
 
 main
