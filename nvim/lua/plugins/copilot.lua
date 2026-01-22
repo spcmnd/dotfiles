5fef9c41a40f08fe90466d0e5ff8d98706f94629
artlt <119579098+artlt@users.noreply.github.com>
2026-01-22 13:29:41 +0100

    Add nvim config (lazyvim)

---
+++ nvim/lua/plugins/copilot.lua
@@ -0,0 +1,12 @@
+return {
+  {
+    "CopilotC-Nvim/CopilotChat.nvim",
+    dependencies = {
+      { "nvim-lua/plenary.nvim", branch = "master" },
+    },
+    build = "make tiktoken",
+    opts = {
+      model = "claude-sonnet-4.5",
+    },
+  },
+}
